e99f3796dbd4aff589a88ccb8193c5b4da01349c
hxxnxxa <ohyunwha@gmail.com>
2022-12-23 21:24:30 +0900

    first commit

---
+++ model.py
@@ -0,0 +1,222 @@
+import tensorflow as tf
+import collections
+import os
+import glob
+
+from ops import *
+
+EPS = 1e-12
+
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+trg_font_path = os.path.join(SCRIPT_PATH, 'tgt_font')
+
+Model = collections.namedtuple("Model", "outputs, predict_real, predict_fake, disc_real_loss, disc_fake_loss, disc_loss_real_styl, discrim_grads_and_vars, gen_loss_GAN, gen_loss_L1, gen_grads_and_vars, train")
+
+# parameters for style embedding
+train_num_styles = len(glob.glob1(trg_font_path,"*.ttf"))
+fine_tune_styles = 1
+total_styles = train_num_styles + fine_tune_styles
+
+##################################################################################
+# Generator
+# It is a U-Net architecture i.e. and encoder decoder with skip connections
+##################################################################################
+
+def create_generator(generator_inputs, generator_outputs_channels, styl_label, args):
+    layers = []
+
+    # encoder_1: [batch, 256, 256, in_channels] => [batch, 128, 128, ngf]
+    with tf.variable_scope("encoder_1"):
+        output = gen_conv(generator_inputs, args.ngf, args)
+        layers.append(output)
+
+    layer_specs = [
+        args.ngf * 2, # encoder_2: [batch, 128, 128, ngf] => [batch, 64, 64, ngf * 2]
+        args.ngf * 4, # encoder_3: [batch, 64, 64, ngf * 2] => [batch, 32, 32, ngf * 4]
+        args.ngf * 8, # encoder_4: [batch, 32, 32, ngf * 4] => [batch, 16, 16, ngf * 8]
+        args.ngf * 8, # encoder_5: [batch, 16, 16, ngf * 8] => [batch, 8, 8, ngf * 8]
+        args.ngf * 8, # encoder_6: [batch, 8, 8, ngf * 8] => [batch, 4, 4, ngf * 8]
+        args.ngf * 8, # encoder_7: [batch, 4, 4, ngf * 8] => [batch, 2, 2, ngf * 8]
+        args.ngf * 8, # encoder_8: [batch, 2, 2, ngf * 8] => [batch, 1, 1, ngf * 8]
+    ]
+
+    for out_channels in layer_specs:
+        with tf.variable_scope("encoder_%d" % (len(layers) + 1)):
+            rectified = lrelu(layers[-1], 0.2)
+            # [batch, in_height, in_width, in_channels] => [batch, in_height/2, in_width/2, out_channels]
+            convolved = gen_conv(rectified, out_channels, args)
+            output = batchnorm(convolved)
+            layers.append(output)
+
+    # Adding style labels here.
+    styl_labels = tf.reshape(styl_label, [-1, 1, 1, total_styles])
+    # We are concatinating styles with enocder output.   
+    layers[-1] = tf.concat([layers[-1], styl_labels], axis=3)
+
+    layer_specs = [
+        (args.ngf * 8, 0.5),   # decoder_8: [batch, 1, 1, ngf * 8] => [batch, 2, 2, ngf * 8 * 2]
+        (args.ngf * 8, 0.5),   # decoder_7: [batch, 2, 2, ngf * 8 * 2] => [batch, 4, 4, ngf * 8 * 2]
+        (args.ngf * 8, 0.5),   # decoder_6: [batch, 4, 4, ngf * 8 * 2] => [batch, 8, 8, ngf * 8 * 2]
+        (args.ngf * 8, 0.0),   # decoder_5: [batch, 8, 8, ngf * 8 * 2] => [batch, 16, 16, ngf * 8 * 2]
+        (args.ngf * 4, 0.0),   # decoder_4: [batch, 16, 16, ngf * 8 * 2] => [batch, 32, 32, ngf * 4 * 2]
+        (args.ngf * 2, 0.0),   # decoder_3: [batch, 32, 32, ngf * 4 * 2] => [batch, 64, 64, ngf * 2 * 2]
+        (args.ngf, 0.0),       # decoder_2: [batch, 64, 64, ngf * 2 * 2] => [batch, 128, 128, ngf * 2]
+    ]
+
+    num_encoder_layers = len(layers)
+    for decoder_layer, (out_channels, dropout) in enumerate(layer_specs):
+        skip_layer = num_encoder_layers - decoder_layer - 1
+        with tf.variable_scope("decoder_%d" % (skip_layer + 1)):
+            if decoder_layer == 0:
+                # first decoder layer doesn't have skip connections
+                # since it is directly connected to the skip_layer
+                input = layers[-1]
+            else:
+                input = tf.concat([layers[-1], layers[skip_layer]], axis=3)
+
+            rectified = tf.nn.relu(input)
+            # [batch, in_height, in_width, in_channels] => [batch, in_height*2, in_width*2, out_channels]
+            output = gen_deconv(rectified, out_channels, args)
+            output = batchnorm(output)
+
+            if dropout > 0.0:
+                output = tf.nn.dropout(output, keep_prob=1 - dropout)
+
+            layers.append(output)
+
+    # decoder_1: [batch, 128, 128, ngf * 2] => [batch, 256, 256, generator_outputs_channels]
+    with tf.variable_scope("decoder_1"):
+        input = tf.concat([layers[-1], layers[0]], axis=3)
+        rectified = tf.nn.relu(input)
+        output = gen_deconv(rectified, generator_outputs_channels, args)
+        output = tf.tanh(output)
+        layers.append(output)
+
+    return layers[-1]
+
+##################################################################################
+# Discriminator
+# Its a PatchGAN with outputs a patch of N*N dimension. N*N here is 30*30
+# Each pixel in the N*N patch is actually telling whether the corresponding patch 
+# in the input image is Real or Fake
+##################################################################################
+
+def create_discriminator(discrim_inputs, discrim_targets, args):
+        n_layers = 3
+        layers = []
+
+        # 2x [batch, height, width, in_channels] => [batch, height, width, in_channels * 2]
+        input = tf.concat([discrim_inputs, discrim_targets], axis=3)
+
+        # layer_1: [batch, 256, 256, in_channels * 2] => [batch, 128, 128, ndf]
+        with tf.variable_scope("layer_1"):
+            convolved = discrim_conv(input, args.ndf, stride=2)
+            rectified = lrelu(convolved, 0.2)
+            layers.append(rectified)
+
+        # layer_2: [batch, 128, 128, ndf] => [batch, 64, 64, ndf * 2]
+        # layer_3: [batch, 64, 64, ndf * 2] => [batch, 32, 32, ndf * 4]
+        # layer_4: [batch, 32, 32, ndf * 4] => [batch, 31, 31, ndf * 8]
+        for i in range(n_layers):
+            with tf.variable_scope("layer_%d" % (len(layers) + 1)):
+                out_channels = args.ndf * min(2**(i+1), 8)
+                stride = 1 if i == n_layers - 1 else 2  # last layer here has stride 1
+                convolved = discrim_conv(layers[-1], out_channels, stride=stride)
+                normalized = batchnorm(convolved)
+                rectified = lrelu(normalized, 0.2)
+                layers.append(rectified)
+
+        # layer_5: [batch, 31, 31, ndf * 8] => [batch, 30, 30, 1]
+        with tf.variable_scope("layer_%d" % (len(layers) + 1)):
+            convolved = discrim_conv(rectified, out_channels=1, stride=1)
+            output = tf.sigmoid(convolved)
+            layers.append(output)
+
+        # Adding Fully connected layer after our Encoder as we want to add classification loss
+        output_flat = tf.layers.flatten(layers[-1])
+
+        with tf.variable_scope('layer_fc_s'):
+            styl_y = tf.layers.dense(output_flat, total_styles,
+                                          kernel_initializer=tf.random_normal_initializer(0, 0.02))
+
+        return layers[-1], styl_y
+
+##################################################################################
+# Build Model
+# Run the Generator, then the discriminator two times for real and fake image respectively. 
+# Two loss functions are used 1) GAN loss 2) L1 loss
+# Then Generator and Discriminator are trained using the Adam Optimizer
+# Then apply ExponentailMovingAverage while training the weights
+# global_step then just keeps track of the number of batches seen so far.
+##################################################################################
+
+def create_model(inputs, targets, style_labels, args):
+    with tf.variable_scope("generator"):
+        out_channels = int(targets.get_shape()[-1])
+        outputs = create_generator(inputs, out_channels, style_labels, args)
+
+    # create two copies of discriminator, one for real pairs and one for fake pairs
+    # they share the same underlying variables
+    with tf.name_scope("real_discriminator"):
+        with tf.variable_scope("discriminator"):
+            # 2x [batch, height, width, channels] => [batch, 7, 7, 1]
+            predict_real, real_styl = create_discriminator(inputs, targets, args)
+
+    with tf.name_scope("fake_discriminator"):
+        with tf.variable_scope("discriminator", reuse=True):
+            # 2x [batch, height, width, channels] => [batch, 7, 7, 1]
+            predict_fake, fake_styl = create_discriminator(inputs, outputs, args)
+
+    # Loss Functions
+    with tf.name_scope("discriminator_loss"):
+        # minimizing -tf.log will try to get inputs to 1
+        # predict_real => 1
+        # predict_fake => 0
+        disc_real_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(predict_real), logits=predict_real))
+        disc_fake_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.zeros_like(predict_fake), logits=predict_fake))
+        
+        disc_loss_real_styl = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=style_labels, logits=real_styl))
+
+        # Discriminator Final Loss
+        discrim_loss =  disc_real_loss + disc_fake_loss + disc_loss_real_styl * args.classification_penalty
+
+    with tf.name_scope("generator_loss"):
+        # predict_fake => 1
+        # abs(targets - outputs) => 0
+        gen_loss_GAN = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(predict_fake), logits=predict_fake))
+        gen_loss_L1 = tf.reduce_mean(tf.abs(targets - outputs))
+        gen_loss = gen_loss_GAN * args.gan_weight + gen_loss_L1 * args.l1_weight
+
+    # Training D and G using Adam
+    with tf.name_scope("discriminator_train"):
+        discrim_tvars = [var for var in tf.trainable_variables() if var.name.startswith("discriminator")]
+        discrim_optim = tf.train.AdamOptimizer(args.lr, args.beta1)
+        discrim_grads_and_vars = discrim_optim.compute_gradients(discrim_loss, var_list=discrim_tvars)
+        discrim_train = discrim_optim.apply_gradients(discrim_grads_and_vars)
+
+    with tf.name_scope("generator_train"):
+        with tf.control_dependencies([discrim_train]):
+            gen_tvars = [var for var in tf.trainable_variables() if var.name.startswith("generator")]
+            gen_optim = tf.train.AdamOptimizer(args.lr, args.beta1)
+            gen_grads_and_vars = gen_optim.compute_gradients(gen_loss, var_list=gen_tvars)
+            gen_train = gen_optim.apply_gradients(gen_grads_and_vars)
+
+    ema = tf.train.ExponentialMovingAverage(decay=0.99)
+    update_losses = ema.apply([disc_real_loss, disc_fake_loss, disc_loss_real_styl, gen_loss_GAN, gen_loss_L1])
+
+    global_step = tf.train.get_or_create_global_step()
+    incr_global_step = tf.assign(global_step, global_step+1)
+
+    return Model(
+        predict_real=predict_real,
+        predict_fake=predict_fake,
+        disc_real_loss=ema.average(disc_real_loss),
+        disc_fake_loss=ema.average(disc_fake_loss),
+        disc_loss_real_styl=ema.average(disc_loss_real_styl),
+        discrim_grads_and_vars=discrim_grads_and_vars,
+        gen_loss_GAN=ema.average(gen_loss_GAN),
+        gen_loss_L1=ema.average(gen_loss_L1),
+        gen_grads_and_vars=gen_grads_and_vars,
+        outputs=outputs,
+        train=tf.group(update_losses, incr_global_step, gen_train),
+    )
